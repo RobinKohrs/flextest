969e462aca80b6f02b1e734bd4c86e0f3cfe306f
RobinKohrs <robin.kohrs@gmx.de>
2023-12-28 15:41:49 +0100

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+</script>
+
+<main>
+  <div class="header">header</div>
+  <div class="main">Main</div>
+</main>
+
+<style>
+  main {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+
+  .header {
+    background-color: red;
+  }
+
+  .main {
+    flex-grow: 1;
+    background-color: green;
+  }
+</style>
